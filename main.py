8aa92332d49c51a30c483a334647889113332aca
Ilya Kodua <kodua.stc@gmail.com>
2022-06-09 20:59:38 +0300

    [+] all done

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import numpy as np
 import numpy as np
 from utils import *
 
-# dir = "ast_embeddings_full/"
-dir = "mobilenetv1_embeddings_full/"
+# a = dict({0:[],1:[]})
+
+# a[0].append(0)
+# a[1].append(1)
+# a[0].append(2)
+
+
+
+dir1 = "ast_embeddings_full/out/"
+dir2 = "mobilenetv1_embeddings_full/out/"
 classes = ["bearing", "fan", "gearbox", "slider", "ToyCar", "ToyTrain", "valve"]
 sections = ["section_00", "section_01", "section_02", "section_03", "section_04", "section_05"]
 
@@ -16,7 +24,20 @@ for cl in classes:
         multy.append(cl + "_" + sc)
 
 
-centrals_embeddings = get_mean_embeddings(dir, multy)
+knn= get_mean_embeddings( multy,  dir1, dir2)
 
-auc, pauc = calculate_anamaly_score(centrals_embeddings, multy, dir)
-print(auc)
+
+auc, pauc = calculate_anamaly_score(knn, multy, dir1, dir2)
+print(1)
+auc_all = 0
+pauc_all = 0
+cnt = 0
+for cl in multy:
+    if auc[cl] != 0:
+       auc_all += auc[cl]
+       pauc_all += pauc[cl]
+       cnt += 1
+auc_all /= cnt    
+pauc_all /= cnt    
+print(auc_all)
+print(pauc_all)
--- utils.py
+++ utils.py
@@ -20,44 +20,70 @@ def get_len_of_embedding(file_path):
         embedding = pickle.load(f)
     return len(embedding)
 
-def get_mean_embeddings(folder, classes):
-    list_files = glob.glob(folder + '**/*.pkl', recursive=True)
-    len_of_embedding = get_len_of_embedding(list_files[0])
-    embeddings_all = dict.fromkeys(classes,  [])
-    labels_all = dict.fromkeys(classes, [])
-    knn_my = dict.fromkeys(classes,  NearestNeighbors(n_neighbors=1,  metric='cosine'))
-    for file_path in list_files:
-        embedding = pickle.load(open(file_path, 'rb'))
+def init_dict_array(classes):
+    embeddings_all = dict()
+
+    for cl in classes:
+        embeddings_all[cl] = []
+    return embeddings_all
+
+def init_dict_zeros(classes):
+    embeddings_all = dict()
+
+    for cl in classes:
+        embeddings_all[cl] = 0
+    return embeddings_all
+
+
+def init_dict_knn(classes):
+    embeddings_all = dict()
+
+    for cl in classes:
+        embeddings_all[cl] = NearestNeighbors(n_neighbors=1,  metric='cosine')
+    return embeddings_all
+
+def get_mean_embeddings(classes, dir1, dir2):
+    list_files = glob.glob(dir1 + '**/*.pkl', recursive=True)
+
+    embeddings_all = init_dict_array(classes)
+
+    knn_my = init_dict_knn(classes)
+    for i,file_path in enumerate(list_files):
         file = file_path.split("/")[-1]
+        embedding1 = pickle.load(open(dir1 + file, 'rb'))
+        embedding2 = pickle.load(open(dir2 + file, 'rb'))
+        embedding = np.concatenate((embedding1,embedding2), axis=0)
         conf = config(file)
         if conf.isTrain and (conf.normal):
-            embeddings_all.append(embedding)
-            labels_all.append(np.where(conf.class_machin == classes))
+            embeddings_all[conf.class_machin].append(embedding)
     
     for cl in classes:
-        knn_my[cl].fit(embeddings_all[cl], labels_all[cl])
+        knn_my[cl].fit(embeddings_all[cl])
 
     return knn_my
 
-def calculate_anamaly_score(knn_my, classes, folder):
+def calculate_anamaly_score(knn, classes, dir1, dir2):
 
-    list_files = glob.glob(folder + '**/*.pkl', recursive=True)
-    anomaly_score = dict.fromkeys(classes,[])
-    auc = dict.fromkeys(classes,0)
-    pauc = dict.fromkeys(classes, 0)
-    labels = dict.fromkeys(classes, [])
-    embeddings_all = dict.fromkeys(classes, [])
-    for file_path in list_files:
-        embedding = pickle.load(open(file_path, 'rb'))
+    list_files1 = glob.glob(dir1 + '**/*.pkl', recursive=True)
+    auc = init_dict_zeros(classes)
+    pauc = init_dict_zeros(classes)
+    labels = init_dict_array(classes)
+    embeddings_all = init_dict_array(classes)
+    for file_path in list_files1:
         file = file_path.split("/")[-1]
+        embedding1 = pickle.load(open(dir1 + file, 'rb'))
+        embedding2 = pickle.load(open(dir2 + file, 'rb'))
+        embedding = np.concatenate((embedding1,embedding2), axis=0)
         conf = config(file)
         if ( not conf.isTrain ) and (conf.normal or conf.anomaly) :
             embeddings_all[conf.class_machin].append(embedding)
             labels[conf.class_machin].append(int(conf.anomaly))
-    
+
     
     for cl in classes:
-            lvl,_ = knn_my[cl].kneighbors(embeddings_all[cl])
+
+        if embeddings_all[cl]:
+            lvl,_ = knn[cl].kneighbors(embeddings_all[cl])
             auc[cl] = metrics.roc_auc_score(labels[cl], lvl)
             pauc[cl] = metrics.roc_auc_score(labels[cl],  lvl, max_fpr = 0.1)
 
